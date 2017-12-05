ba7d2263bb4f85dae895a0281ec69f834bfba6bb
seraphli <seraphlivery@gmail.com>
2017-12-05 11:41:25 +0800

    update enemy crawler

--- README.md
+++ README.md
@@ -6,4 +6,8 @@ Web crawler using Scrapy.
 
 1. pip install scrapy
 2. cd kr
-2. scrapy crawl tower -o tower.json
+3. scrapy crawl tower -o tower.json
+4. scrapy crawl enemy -o enemy.json
+
+Use `scrapy shell "http://quotes.toscrape.com/page/1/"` to run
+Scrapy shell from command-line
--- kr/kr/spiders/enemy.py
+++ kr/kr/spiders/enemy.py
@@ -9,16 +9,20 @@ class EnemySpider(scrapy.Spider):
     enemy_selection = [2, 3]
 
     def parse(self, response):
+        enemy_id = 0
         for t in self.enemy_selection:
             enemy_url = response.css("h3+table")[t].css("div+a::attr(href)").extract()
             enemy_type = response.css("h3 span.mw-headline::text")[t].extract()
             self.log("Find %d %s." % (len(enemy_url), enemy_type))
             for next_page_url in enemy_url:
                 request = response.follow(next_page_url, callback=self.parse_enemy)
+                enemy_id += 1
+                request.meta["enemy_id"] = enemy_id
                 request.meta["enemy_type"] = enemy_type
                 yield request
 
     def parse_enemy(self, response):
+        enemy_id = response.meta["enemy_id"]
         enemy_type = response.meta["enemy_type"]
         title = response.css(".page-header__title::text").extract_first()
         attr_key = response.css(".pi-data-label.pi-secondary-font b::text").extract()
@@ -28,6 +32,7 @@ class EnemySpider(scrapy.Spider):
             attr_value = response.css("div.center+table").css("td::text").extract()
         yield {
             "title": title,
+            "enemy_id": enemy_id,
             "enemy_type": enemy_type,
             "attribute": dict(zip(attr_key, attr_value))
         }
