98715d4e7889c4e07e97c66a6d38194c25858b4a
seraphli <seraphlivery@gmail.com>
2017-12-05 15:36:05 +0800

    update tower crawler

--- kr/kr/spiders/tower.py
+++ kr/kr/spiders/tower.py
@@ -10,28 +10,40 @@ class TowerSpider(scrapy.Spider):
     upgrade_selection = 1
 
     def parse(self, response):
+        tower_id = 0
         for t in self.tower_selection:
             tower_url = response.css("h3+table")[t].css("div+a::attr(href)").extract()
             tower_type = response.css("h3 span.mw-headline::text")[t].extract()
             self.log("Find %d %s." % (len(tower_url), tower_type))
             for next_page_url in tower_url:
                 request = response.follow(next_page_url, callback=self.parse_tower)
+                tower_id += 1
+                request.meta["tower_id"] = tower_id
                 request.meta["tower_type"] = tower_type
                 yield request
 
     def parse_tower(self, response):
+        tower_id = response.meta["tower_id"]
         tower_type = response.meta["tower_type"]
         title = response.css(".page-header__title::text").extract_first()
-        attr_key = response.css("div.center+table").css("td b::text").extract()
-        if response.css("span#Upgrades::text").extract():
-            attr_value = response.css("div.center+table").css("td::text").extract()[:len(attr_key) - 1]
-            upgrade = [response.css("h3+p a::text").extract()[self.upgrade_selection],
-                       response.css("h3+p+p a::text").extract()[self.upgrade_selection]]
-            attr_value += [upgrade]
+        attr_key = response.css("div h3 b::text").extract()
+        del attr_key[attr_key.index("Introduced")]
+        del attr_key[attr_key.index("Upgrades")]
+        attr_value = response.css("div.pi-data-value.pi-font::text").extract()
+        attribute = dict(zip(attr_key, attr_value))
+        sup = response.css("div div sup::text").extract()
+        sup_text = response.css("div.pi-data-value.pi-font a::text").extract()
+        KRF_info = []
+        for i in range(len(sup)):
+            if sup[i] == "KRF":
+                KRF_info.append(sup_text[i])
+        if len(sup) < len(sup_text):
+            attribute.update({"Introduced": KRF_info[0], "Upgrades": sup_text[-1]})
         else:
-            attr_value = response.css("div.center+table").css("td::text").extract()
+            attribute.update({"Introduced": KRF_info[0], "Upgrades": KRF_info[1:]})
         yield {
             "title": title,
+            "tower_id": tower_id,
             "tower_type": tower_type,
-            "attribute": dict(zip(attr_key, attr_value))
+            "attribute": attribute
         }
